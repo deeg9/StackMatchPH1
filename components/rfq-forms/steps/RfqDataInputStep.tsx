f520fa5bac6d6f7d900e6533cc1bd6560ef1f7f8
Diego Fill <christopherfill9@gmail.com>
2025-07-24 20:34:33 -0700

    Update to RFQ page

--- components/rfq-forms/steps/RfqDataInputStep.tsx
+++ components/rfq-forms/steps/RfqDataInputStep.tsx
@@ -113,7 +113,7 @@ export function RfqDataInputStep({
       </div>
 
       {/* Input Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="space-y-6">
         {/* Company Website Card */}
         <Card className="p-6 space-y-4">
           <div className="flex items-center gap-3">
